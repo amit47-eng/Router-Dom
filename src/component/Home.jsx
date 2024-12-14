02f43cd426c2f1bd2c78fd8ffedb1b961ac12f41
amit47-eng <av495267@gmail.com>
2024-12-14 22:37:37 +0530

    adding the about.css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,3 +1,5 @@
+import "./style/home.css"
+
 function Home(){
     return <h4 className="home">Home</h4>
 }
--- src/component/about.jsx
+++ src/component/about.jsx
@@ -1,5 +1,7 @@
+import "./style/about.css"
+
 function About(){
-    return <h4>about</h4>
+    return <h4 className="about">about</h4>
 }
 
 export default About;
