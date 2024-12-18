d5f5aa7cef8432428b2424146477f18fd45ca5c3
amit47-eng <av495267@gmail.com>
2024-12-18 22:40:50 +0530

    adding a input tag

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,13 @@
 import "./style/home.css"
 
 function Home(){
-    return <h4 className="home">Home</h4>
+    return (
+    <div><h4 className="home">Home</h4>
+    <input type="text" />
+    <button>click</button>
+    
+    </div>
+    )
 }
 
 export default Home;
